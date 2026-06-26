4e8da98c384eac352a3d6aa320eab55c53df6e55
pyrotechnic <xiaochenl894@gmail.com>
2026-06-26 14:14:47 +0800

    Initial Commit

---
+++ MarkdownReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarkdownReader
+//
+//  Created by 红尘一笑 on 2026/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarkdownReader/MarkdownReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownReaderApp.swift
+//  MarkdownReader
+//
+//  Created by 红尘一笑 on 2026/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
